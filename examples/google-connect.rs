c077534a036b2ec796dd96b59ba67cb81d8e45de
webb.shi <bradyjoestar@gmail.com>
2020-05-27 16:59:25 +0800

    temp change the url for cn

--- examples/google-connect.rs
+++ examples/google-connect.rs
@@ -7,8 +7,8 @@ use std::net::TcpStream;
 fn main() {
     let connector = TlsConnector::new().unwrap();
 
-    let stream = TcpStream::connect("google.com:443").unwrap();
-    let mut stream = connector.connect("google.com", stream).unwrap();
+    let stream = TcpStream::connect("sogou.com:443").unwrap();
+    let mut stream = connector.connect("sogou.com", stream).unwrap();
 
     stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
     let mut res = vec![];
